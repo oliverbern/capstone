0bbd7115670d9e3d55b43f93ba6f219fb2c59a39
oliverbern <oapjones@gmail.com>
2014-11-27 19:27:43 +0100

    fixed xls import for colorgems

--- app/models/colorgem.rb
+++ app/models/colorgem.rb
@@ -13,11 +13,34 @@ class Colorgem < ActiveRecord::Base
 	end
 
 	def self.import(file)
-		CSV.foreach(file.path, headers: true) do |row|
+		#CSV.foreach(file.path, headers: true) do |row|
 			#colorgem = find_by_id(row["id"]) || new
 			#colorgem.attributes = row.to_hash.slice(*accessible_attributes)
 			#colorgem.save!
-			Colorgem.create! row.to_hash
+			#Colorgem.create! row.to_hash
+		#end
+
+
+		spreadsheet = open_spreadsheet(file)
+		header = spreadsheet.row(1)
+		(2..spreadsheet.last_row).each do |i|
+			row = Hash[[header, spreadsheet.row(i)].transpose]
+			colorgem = find_by_id(row["id"]) || new
+			colorgem.attributes = row.to_hash.slice(*Colorgem.attribute_names())
+			colorgem.save!
 		end
 	end
+
+	def self.open_spreadsheet(file)
+		case File.extname(file.original_filename)
+		when '.csv' then CSV.new(file.path, nil, :ignore)
+		when '.xls' then Roo::Excel.new(file.path, nil, :ignore)
+		when '.xlsx' then Excelx.new(file.path, nil, :ignore)
+		else raise "Unknown file type: #{file.original_filename}"
+		end
+	end
+
+
+
+
 end
