dfe1ff4396c714c7f3520f44314c32c33ba61a0a
oliverbern <oapjones@gmail.com>
2014-12-01 21:41:36 +0100

    views colorgems

--- app/models/colorgem.rb
+++ app/models/colorgem.rb
@@ -3,7 +3,7 @@ class Colorgem < ActiveRecord::Base
 
 	
 
-	has_attached_file :image, :styles => { :medium => "50x50", :thumb => "100x100" }, :default_url => "default.png"
+	has_attached_file :image, :styles => { :medium => "50x50", :thumb => "100x100", :large => "300x300" }, :default_url => "default.png"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 
