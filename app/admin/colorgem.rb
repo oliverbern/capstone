5befd3efc69d94b22d0c9d0a718b8a5488fe2049
oliverbern <oapjones@gmail.com>
2014-12-01 15:50:25 +0100

    fix1 heroku error activeadmin

--- app/admin/colorgem.rb
+++ app/admin/colorgem.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Colorgem do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params *Colorgem.columns.map(&:name).map(&:to_sym)
+  #permit_params *Colorgem.columns.map(&:name).map(&:to_sym)
   #
   # or
   #
