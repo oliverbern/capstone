c603ce05cc79ac3b1ea4ab5418a63711f0d2fcdc
oliverbern <oapjones@gmail.com>
2015-01-05 23:35:53 +0100

    customise homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
   //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require autocomplete-rails
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require autocomplete-rails
 //= require_tree .
