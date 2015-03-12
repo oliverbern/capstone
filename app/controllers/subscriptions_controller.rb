7b931d4620b55d52c3c215ff5940c571c5a90a1c
oliverbern <oapjones@gmail.com>
2015-03-12 09:56:51 +0100

    change subscription controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,20 +4,10 @@ class SubscriptionsController < ApplicationController
 
 		current_user.create_customer(params[:customer])
 		
-
-
-
-
-		# @subscription = current_user.subscription.build(params[:subscription])
-		# if subscription.save
-		# 	redirect_to root_path
-		# else
-		# 	render :new
-		# end
 	end
 
 	private 
 
 	def colorgem_params
-      params.require(:subscription).permit(:plan, :card,)
+      params.require(:subscription).permit(:plan, :card)
 end
